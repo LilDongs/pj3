a6f7db7f99971a6771b0e8344bda7049d72959b2
Ally <89672479+ayly8@users.noreply.github.com>
2021-10-24 00:32:48 -0700

    AHA

--- BinaryTree.java
+++ BinaryTree.java
@@ -144,7 +144,10 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The height of the "whole" tree. */
    public int getHeight_callBinaryNodeMethod()
    {
-      return 0;
+      int height = 0;
+      if (root != null)
+         height = root.getHeight_binaryNodeMethod();
+	   return height;
    } // end getHeight_callBinaryNodeMethod
 
    /** -------------------------------------------------------------------- */
