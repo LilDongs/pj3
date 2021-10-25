f7d9e37a1001fddf54ac992a2120dad5c59c2463
Ally <89672479+ayly8@users.noreply.github.com>
2021-10-24 22:57:14 -0700

    we finished task 1

--- BinaryTree.java
+++ BinaryTree.java
@@ -111,6 +111,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
     * prints (using post-order traversal) all nodes in the "whole" tree */
    public void postorderTraverse_callBinaryNodeMethod()
    {
+      root.postorderTraverse_binaryNodeMethod();
    }
    
    /** -------------------------------------------------------------------- */
